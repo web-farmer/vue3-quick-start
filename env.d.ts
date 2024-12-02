6ae305b8bb2f5a3a640a4e7f985cfbaee25a5d14
forguo <wforguo@qq.com>
2024-12-02 14:26:57 +0800

    feat: 实现图片对比及缩放

--- env.d.ts
+++ env.d.ts
@@ -13,3 +13,13 @@ interface ImportMeta {
     readonly env: ImportMetaEnv
     readonly glob: (path: string, config: object) => Record<string, () => Promise<{ default: any }>>
 }
+
+declare module 'image-compare-viewer' {
+    interface ImageCompareOptions {}
+
+    export default class ImageCompare {
+        constructor(element: HTMLElement | null, options?: any)
+        mount(): this // 如果 `mount` 方法返回实例本身
+        // 如果返回值不同，可以替换 `this` 为实际的返回类型。
+    }
+}
